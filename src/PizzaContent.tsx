b9661eafdb365f69c6b78990f385ff7671a61aca
Amange1di <amangeldi2702006@gmail.com>
2024-07-10 17:43:01 +0600

    modified:   src/PizzaContent.tsx

--- src/PizzaContent.tsx
+++ src/PizzaContent.tsx
@@ -11,19 +11,19 @@ const dataPizza:IDataPizza[] = [
         name: "Креветки со сладким чили",
         desc: "Креветки, ананасы, соус сладкий чили, сладкий перец, моцарелла, соус альфредо",
         img: "https://media.dodostatic.net/image/r:292x292/11EF33DC750FF17794FB1ACA35F22A6E.avif",
-        price: "545",
+        price: 545,
     },
     {
         name: "Баварская",
         desc: "Охотничьи колбаски, маринованные огурчики, красный лук, томаты, горчичный соус, моцарелла, томатный соус",
         img: "https://media.dodostatic.net/image/r:292x292/11EF346408A39615BD3478135F9F6673.avif",
-        price: "495",
+        price: 495,
     },
     {
         name: "Чикен Бомбони",
         desc: "Куриные кусочки в панировке, сладкий перчик, сыры чеддер, пармезан и моцарелла, красный лук, соусы сладкий чили и альфредо",
         img: "https://media.dodostatic.net/image/r:292x292/11EF3DB2188200718EDEB6D5EC3E3ECD.avif",
-        price: "555",
+        price: 555,
     },
 ]
 const PizzaContent = () => {
